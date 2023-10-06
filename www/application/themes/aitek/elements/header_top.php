f596eae12db8f7821f5453816de552de868d87dc
Ari <arilankinen4@gmail.com>
2023-10-06 12:59:45 +0300

    adding fav icon

--- www/application/themes/aitek/elements/header_top.php
+++ www/application/themes/aitek/elements/header_top.php
@@ -69,8 +69,7 @@
     </script>
 
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="icon" href="/application/files/6016/9564/6350/favicon_Aitek_.png.ico">
     <link rel="manifest" href="/site.webmanifest">
     <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
     <meta name="msapplication-TileColor" content="#00aba9">
